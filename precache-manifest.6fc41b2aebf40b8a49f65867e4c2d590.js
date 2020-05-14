e855468adff51de50d8c04df050109dbe667158f
mk1120 <kitmk1120@gmail.com>
2020-05-14 21:47:51 +0800

    Updates

---
+++ precache-manifest.6fc41b2aebf40b8a49f65867e4c2d590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e357070279e706ab3207ebc078f1f117",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "3909a52302258a1e6b6d",
+    "url": "/monsters/static/css/main.d3aa8562.chunk.css"
+  },
+  {
+    "revision": "2cb572492de21cf85d46",
+    "url": "/monsters/static/js/2.3a13a80b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.3a13a80b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3909a52302258a1e6b6d",
+    "url": "/monsters/static/js/main.e7d4232b.chunk.js"
+  },
+  {
+    "revision": "9fe26802fb5126ce503d",
+    "url": "/monsters/static/js/runtime-main.dcd40a9b.js"
+  }
+]);
